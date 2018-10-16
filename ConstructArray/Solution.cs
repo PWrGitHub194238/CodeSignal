55531856988732621596391f7ee8f22973832ed0
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-16 13:31:53 +0200

    ConstructArray - solution.

--- ConstructArray/Solution.cs
+++ ConstructArray/Solution.cs
@@ -1,11 +1,25 @@
-﻿
-
-namespace ConstructArray
+﻿namespace ConstructArray
 {
     public class Solution
     {
-        public static int[] constructArray(int size) {
-            return (int[]) new object();
+        public static int[] constructArray(int size)
+        {
+            int idx;
+            int val = 1;
+            int[] array = new int[size];
+
+            for (idx = 1; idx < size; idx += 2)
+            {
+                array[idx - 1] = val;
+                array[idx] = size - (val - 1);
+                val += 1;
+            }
+
+            if (idx == size)
+            {
+                array[size - 1] = val;
+            }
+            return array;
         }
     }
 }
