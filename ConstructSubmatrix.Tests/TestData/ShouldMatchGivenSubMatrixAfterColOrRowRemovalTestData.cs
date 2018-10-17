e81e510584f9f121ed4dcd2ceb2e25e7ddc5b933
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-17 17:14:11 +0200

    ConstructSubmatrix - solution.

--- ConstructSubmatrix.Tests/TestData/ShouldMatchGivenSubMatrixAfterColOrRowRemovalTestData.cs
+++ ConstructSubmatrix.Tests/TestData/ShouldMatchGivenSubMatrixAfterColOrRowRemovalTestData.cs
@@ -7,9 +7,19 @@ namespace ConstructSubmatrix.Tests.TestData
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-			// Inputs: int[][] matrix, int[] rowsToDelete, int[] columnsToDelete
-			// Outputs: int[][]
-            yield return new object[] { };
+            // Inputs: int[][] matrix, int[] rowsToDelete, int[] columnsToDelete
+            // Outputs: int[][]
+            yield return new object[] {
+                new int[][] {
+                    new int[] { 1, 0, 0, 2 },
+                    new int[] { 0, 5, 0, 1 },
+                    new int[] { 0, 0, 3, 5 }
+                }, new int[] { 1 }, new int[] { 0, 2 },
+                new int[][] {
+                    new int[] { 0, 2 },
+                    new int[] { 0, 5 }
+                }
+            };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
--- ConstructSubmatrix/Solution.cs
+++ ConstructSubmatrix/Solution.cs
@@ -1,11 +1,101 @@
 ﻿
+using System;
+
 namespace ConstructSubmatrix
 {
     public class Solution
     {
         public static int[][] constructSubmatrix(int[][] matrix, int[] rowsToDelete, int[] columnsToDelete)
         {
-            return (int[][])new object();
+            int numberOfRows = matrix.Length;
+            int numberOfColumns = matrix[0].Length;
+            int numberOfRowsToBeDeleted = rowsToDelete.Length;
+            int numberOfColumnsToBeDeleted = columnsToDelete.Length;
+            int numberOfRowsInSubMatrix = numberOfRows - numberOfRowsToBeDeleted;
+            int numberOfColumnsInSubMatrix = numberOfColumns - numberOfColumnsToBeDeleted;
+
+            int[][] submatrix = new int[numberOfRowsInSubMatrix][];
+
+            Array.Sort(rowsToDelete);
+            Array.Sort(columnsToDelete);
+
+            int i = 0;
+            int iSubIdx = 0;
+            int rowIdxToDelete = 0;
+
+            int j;
+            int jSubIdx = 0;
+            int columnIdxToDelete;
+            int[] tmpCollumn;
+            while (i < numberOfRows && rowIdxToDelete < numberOfRowsToBeDeleted && iSubIdx < numberOfRowsInSubMatrix)
+            {
+                if (i != rowsToDelete[rowIdxToDelete])
+                {
+                    j = 0;
+                    jSubIdx = 0;
+                    columnIdxToDelete = 0;
+                    tmpCollumn = new int[numberOfColumnsInSubMatrix];
+                    while (j < numberOfColumns && columnIdxToDelete < numberOfColumnsToBeDeleted && jSubIdx < numberOfColumnsInSubMatrix)
+                    {
+                        if (j != columnsToDelete[columnIdxToDelete])
+                        {
+                            tmpCollumn[jSubIdx] = matrix[i][j];
+                            jSubIdx += 1;
+                        }
+                        else
+                        {
+                            columnIdxToDelete += 1;
+                        }
+                        j += 1;
+                    }
+                    while (jSubIdx < numberOfColumnsInSubMatrix)
+                    {
+                        tmpCollumn[jSubIdx] = matrix[i][j];
+                        j += 1;
+                        jSubIdx += 1;
+                    }
+                    submatrix[iSubIdx] = tmpCollumn;
+                    iSubIdx += 1;
+                }
+                else
+                {
+                    rowIdxToDelete += 1;
+                }
+                i += 1;
+            }
+
+            while (iSubIdx < numberOfRowsInSubMatrix)
+            {
+                j = 0;
+                jSubIdx = 0;
+                columnIdxToDelete = 0;
+                tmpCollumn = new int[numberOfColumnsInSubMatrix];
+                while (j < numberOfColumns && columnIdxToDelete < numberOfColumnsToBeDeleted && jSubIdx < numberOfColumnsInSubMatrix)
+                {
+                    if (j != columnsToDelete[columnIdxToDelete])
+                    {
+                        tmpCollumn[jSubIdx] = matrix[i][j];
+                        jSubIdx += 1;
+                    }
+                    else
+                    {
+                        columnIdxToDelete += 1;
+                    }
+                    j += 1;
+                }
+                while (jSubIdx < numberOfColumnsInSubMatrix)
+                {
+                    tmpCollumn[jSubIdx] = matrix[i][j];
+                    j += 1;
+                    jSubIdx += 1;
+                }
+
+
+                submatrix[iSubIdx] = tmpCollumn;
+                i += 1;
+                iSubIdx += 1;
+            }
+            return submatrix;
         }
     }
 }
