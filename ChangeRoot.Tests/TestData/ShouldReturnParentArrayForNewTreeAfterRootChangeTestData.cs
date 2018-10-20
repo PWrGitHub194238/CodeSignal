b9ae5e3afaf65ea3db917f39f612f2a4262de99c
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-20 17:07:10 +0200

    ChangeRoot - solution.

--- ChangeRoot.Tests/TestData/ShouldReturnParentArrayForNewTreeAfterRootChangeTestData.cs
+++ ChangeRoot.Tests/TestData/ShouldReturnParentArrayForNewTreeAfterRootChangeTestData.cs
@@ -7,9 +7,12 @@ namespace ChangeRoot.Tests.TestData
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-			// Inputs: int[] parent, int newRoot
-			// Outputs: int[]
-            yield return new object[] { };
+            // Inputs: int[] parent, int newRoot
+            // Outputs: int[]
+            yield return new object[] { new int[] { 0, 0, 0, 1 }, 1, new int[] { 1, 1, 0, 1 } };
+            yield return new object[] { new int[] { 0, 0, 0, 1, 1, 1, 2, 2, 7 }, 7, new int[] { 2, 0, 7, 1, 1, 1, 2, 7, 7 } };
+            yield return new object[] { new int[] { 0, 0 }, 1, new int[] { 1, 1 } };
+            yield return new object[] { new int[] { 0 }, 0, new int[] { 0 } };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
--- ChangeRoot/Solution.cs
+++ ChangeRoot/Solution.cs
@@ -5,7 +5,23 @@ namespace ChangeRoot
     {
         public static int[] changeRoot(int[] parent, int newRoot)
         {
-            return (int[])new object();
+            int currentNode;
+            int parentNode = parent[newRoot];
+            int oldParentNode = parent[parentNode];
+
+            parent[parentNode] = newRoot;
+
+            while (parentNode != oldParentNode)
+            {
+                currentNode = parentNode;
+                parentNode = oldParentNode;
+                oldParentNode = parent[parentNode];
+                parent[parentNode] = currentNode;
+            }
+
+            parent[newRoot] = newRoot;
+
+            return parent;
         }
     }
 }
