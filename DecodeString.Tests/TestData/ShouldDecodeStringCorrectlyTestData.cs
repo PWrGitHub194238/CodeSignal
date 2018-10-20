b197e6394b4ae027abfbc6f6d63103e1a9e815e0
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-20 13:18:34 +0200

    DecodeString - solution (30/31 test passed).

--- DecodeString.Tests/TestData/ShouldDecodeStringCorrectlyTestData.cs
+++ DecodeString.Tests/TestData/ShouldDecodeStringCorrectlyTestData.cs
@@ -7,9 +7,12 @@ namespace DecodeString.Tests.TestData
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-			// Inputs: string s
-			// Outputs: string
-            yield return new object[] { };
+            // Inputs: string s
+            // Outputs: string
+            yield return new object[] { "4[ab]", "abababab" };
+            yield return new object[] { "2[b3[a]]", "baaabaaa" };
+            yield return new object[] { "z1[y]zzz2[abc]", "zyzzzabcabc" };
+            yield return new object[] { "100[codesignal]", "codesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignalcodesignal" };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
--- DecodeString/Solution.cs
+++ DecodeString/Solution.cs
@@ -1,11 +1,74 @@
 ﻿
+using System.Collections;
+using System.Collections.Generic;
+
 namespace DecodeString
 {
     public class Solution
     {
         public static string decodeString(string s)
         {
-            return (string)new object();
+            string decodedString = string.Empty;
+
+            Stack<char> parsingStack = new Stack<char>();
+            Stack<int> multiplierStack = new Stack<int>();
+            Stack<int> localMultiplierStack = new Stack<int>();
+            int multiplier = 1;
+            string lms = "";
+
+            char currentChar;
+            char[] inputCharArray = s.ToCharArray();
+            int inputCharIdx = 0;
+            int inputCharMaxIdx = inputCharArray.Length;
+            string localString;
+
+            while(inputCharIdx < inputCharMaxIdx)
+            {
+                currentChar = inputCharArray[inputCharIdx];
+
+                if (char.IsLetter(currentChar))
+                {
+                    parsingStack.Push(currentChar);
+                } else if (char.IsDigit(currentChar))
+                {
+                    localMultiplierStack.Push(currentChar - 48);
+                } else if (currentChar == '[')
+                {
+                    parsingStack.Push(currentChar);
+
+                    while (localMultiplierStack.Count > 0)
+                    {
+                        lms = localMultiplierStack.Pop() + lms;
+                    }
+                    multiplierStack.Push(int.Parse(lms));
+                    lms = "";
+
+                } else if (currentChar == ']')
+                {
+                    localString = string.Empty;
+                    while (parsingStack.Peek() != '[')
+                    {
+                        localString = parsingStack.Pop() + localString;
+                    }
+                    parsingStack.Pop();
+
+                    multiplier = multiplierStack.Pop();
+                    for (int i = 0; i < multiplier; i++)
+                    {
+                        foreach (var inputChar in localString.ToCharArray())
+                        {
+                            parsingStack.Push(inputChar);
+                        }
+                    }
+                }
+
+                inputCharIdx += 1;
+            }
+
+            while (parsingStack.Count > 0) {
+                decodedString = parsingStack.Pop() + decodedString;
+            }
+            return decodedString;
         }
     }
 }
