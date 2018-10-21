edb7fd908b4162dce0dedf7943800a59fcb6057a
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-21 14:05:06 +0200

    PeriodicSequence - solution.

--- PeriodicSequence.Tests/TestData/ShouldFoundMinimalPeriodTestData.cs
+++ PeriodicSequence.Tests/TestData/ShouldFoundMinimalPeriodTestData.cs
@@ -7,12 +7,19 @@ namespace PeriodicSequence.Tests.TestData
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-			// Inputs: int s0, int a, int b, int m
-			// Outputs: int
-            yield return new object[] { };
+            // Inputs: int s0, int a, int b, int m
+            // Outputs: int
+            yield return new object[] { 11, 2, 6, 12, 2 };
+            yield return new object[] { 1, 2, 3, 5, 4 };
+            yield return new object[] { 0, 0, 0, 1, 1 };
+            yield return new object[] { 6, 0, 1, 7, 1 };
+            yield return new object[] { 10, 0, 1, 17, 1 };
+            yield return new object[] { 6, 0, 2, 7, 1 };
+            yield return new object[] { 6, 1, 1, 7, 7 };
+            yield return new object[] { 6, 1, 2, 7, 7};
+            yield return new object[] { 6, 1, 3, 7, 7 };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
-
--- PeriodicSequence/Solution.cs
+++ PeriodicSequence/Solution.cs
@@ -5,7 +5,16 @@ namespace PeriodicSequence
     {
         public static int periodicSequence(int s0, int a, int b, int m)
         {
-            return (int)new object();
+            int[] isPresent = new int[m];
+            int idx = 0;
+            int s_idx = s0;
+            do
+            {
+                isPresent[s_idx] = idx;
+                idx += 1;
+                s_idx = (a * s_idx + b) % m;
+            } while (isPresent[s_idx] == 0);
+            return idx - isPresent[s_idx];
         }
     }
 }
