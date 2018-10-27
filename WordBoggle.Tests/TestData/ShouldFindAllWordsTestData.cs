86b8d1cf122c57f69b7cfe9b597d924dd27e1704
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-27 17:06:23 +0200

    WordBoggle - solution.

--- WordBoggle.Tests/TestData/ShouldFindAllWordsTestData.cs
+++ WordBoggle.Tests/TestData/ShouldFindAllWordsTestData.cs
@@ -3,13 +3,121 @@ using System.Collections.Generic;
 
 namespace WordBoggle.Tests.TestData
 {
-    class ShouldFindAllWordsTestData : IEnumerable<object[]>
+    internal class ShouldFindAllWordsTestData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-			// Inputs: char[][] board, string[] words
-			// Outputs: string[]
-            yield return new object[] { new char[][] { }, new string[] { }, new string[] { } };
+            // Inputs: char[][] board, string[] words
+            // Outputs: string[]
+            yield return new object[] { new char[][] {
+                new char[] { 'R', 'L', 'D' },
+                new char[] { 'U', 'O', 'E' },
+                new char[] { 'C', 'S', 'O' }
+            }, new string[] { "CODE", "SOLO", "RULES", "COOL" },
+            new string[] { "CODE", "RULES" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'X', 'V', 'W' },
+                new char[] { 'A', 'L', 'T' ,'I' },
+                new char[] { 'T', 'T', 'J', 'R' }
+            }, new string[] { "AXOLOTL", "TAXA", "ABA", "VITA", "VITTA", "GO", "AXAL", "LATTE", "TALA", "RJ" },
+                new string[] { "AXAL", "RJ", "TALA", "TAXA", "VITTA" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B' },
+                new char[] { 'C', 'D' }
+            }, new string[] { },
+                new string[] { } };
+            yield return new object[] { new char[][] {
+            }, new string[] { },
+                new string[] { } };
+            yield return new object[] { new char[][] {
+            }, new string[] { "A", "B","C" },
+                new string[] { } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A' }
+            }, new string[] { "A" },
+                new string[] { "A" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B', }
+            }, new string[] { "A", "B", "C", "AA", "AB", "AC", "BA", "BB", "BC", "CA", "CB", "CC" },
+                new string[] { "A", "AB", "B", "BA" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B' },
+                new char[] { 'C', 'D' }
+            }, new string[] { "A", "AA", "ABCD", "DABC", "BCDA", "CDAB" },
+                new string[] { "A", "ABCD", "BCDA", "CDAB", "DABC" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B' },
+                new char[] { 'C', 'D' }
+            }, new string[] {  "BD", "A", "B", "A", "B", "BD" },
+                new string[] { "A", "B", "BD" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B', 'C', 'D' }
+            }, new string[] { "C" },
+                new string[] { "C" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B', 'C', 'D' }
+            }, new string[] { "CB" },
+                new string[] { "CB" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B', 'C', 'D' }
+            }, new string[] { "BD" },
+                new string[] { } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B', 'C', 'D' }
+            }, new string[] { "DA" },
+                new string[] { } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B', 'C', 'D' }
+            }, new string[] { "AD" },
+                new string[] { } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'A', 'A', 'A' }
+            }, new string[] { "A", "AA", "AAA", "AAAA", "AAAAA" },
+                new string[] { "A", "AA", "AAA", "AAAA" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'A', 'B', 'C', 'D' }
+            }, new string[] { "B", "B", "B", "B" },
+                new string[] { "B" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'B' }
+            }, new string[] { "B", "B", "B", "B" },
+                new string[] { "B" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'B' },
+                new char[] { 'B' },
+                new char[] { 'B' },
+                new char[] { 'B' }
+            }, new string[] { "B", "B", "B", "B" },
+                new string[] { "B" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'B' },
+                new char[] { 'B' },
+                new char[] { 'B' },
+                new char[] { 'B' }
+            }, new string[] { "B", "BB", "BBB", "BBBB", "BBBBB" },
+                new string[] { "B", "BB", "BBB", "BBBB" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'C', 'C' },
+                new char[] { 'C', 'C' },
+            }, new string[] { "CCCC", "CCC", "CC", "C", "CCCCC", "CC", "C", "CCC", "CCCC", "CC", "C" },
+                new string[] { "C", "CC", "CCC", "CCCC" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'C', 'C', 'C' },
+                new char[] { 'C', 'C', 'C' },
+                new char[] { 'C', 'C', 'C' },
+            }, new string[] { "CCCCCCCCC", "CCCCCCCC", "CCCCCCC", "CCCCCC", "CCCCC", "CCCC", "CCC", "CC", "C", "CC", "C", "CCCCCCCCCC" },
+                new string[] { "C", "CC", "CCC", "CCCC", "CCCCC", "CCCCCC", "CCCCCCC", "CCCCCCCC", "CCCCCCCCC" } };
+            yield return new object[] { new char[][] {
+                new char[] { 'C', 'C', 'C', 'C' },
+                new char[] { 'C', 'C', 'C', 'C' },
+                new char[] { 'C', 'C', 'C', 'C' },
+                new char[] { 'C', 'C', 'C', 'C' },
+            }, new string[] { "CCCCCCCCCCCCCCCCC", "CCCCCCCCCCCCCCCC", "CCCCCCCCCCCCCCC", "CCCCCCCCCCCCCC", "CCCCCCCCCCCCC",
+                "CCCCCCCCCCCCC", "CCCCCCCCCCCC","CCCCCCCCCCC", "CCCCCCCCCC", "CCCCCCCCC", "CCCCCCCC", "CCCCCCC", "CCCCCC",
+                "CCCCC", "CCCC", "CCC", "CC", "C", "CC", "CCC", "CCCC", "CCCCCC", "CCCCCCCCCCCCCCCCCC", "CCCCCCCC", "CCC" },
+                new string[] { "C", "CC", "CCC", "CCCC", "CCCCC", "CCCCCC", "CCCCCCC", "CCCCCCCC",
+                    "CCCCCCCCC", "CCCCCCCCCC", "CCCCCCCCCCC", "CCCCCCCCCCCC", "CCCCCCCCCCCCC",
+                    "CCCCCCCCCCCCCC", "CCCCCCCCCCCCCCC", "CCCCCCCCCCCCCCCC" } };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
--- WordBoggle/Solution.cs
+++ WordBoggle/Solution.cs
@@ -1,11 +1,144 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace WordBoggle
 {
     public class Solution
     {
         public static string[] wordBoggle(char[][] board, string[] words)
         {
-            return (string[])new object();
+            if (board.Length == 0) return new string[] { };
+            if (words.Length == 0) return words;
+            if (words.Length > 1)
+            {
+                Array.Sort(words);
+                IList<string> tmp = new List<string>();
+
+                for (int i = 1; i < words.Length; i++)
+                {
+                    if (!words[i - 1].Equals(words[i]))
+                    {
+                        tmp.Add(words[i - 1]);
+                    }
+                }
+
+                if (!words[words.Length - 2].Equals(words.Length - 1))
+                {
+                    tmp.Add(words[words.Length - 1]);
+                }
+
+                words = tmp.ToArray();
+            }
+
+            IList<string> assemblyableWordList = new List<string>();
+            int boardHeight = board.Length;
+            int boardWidth = board[0].Length;
+
+            IList<(int x, int y)>[] charToPositionMappingArray = new IList<(int x, int y)>['Z' - 'A' + 1];
+            bool[][] gotThere = new bool[boardHeight][];
+
+            for (int i = 0; i < charToPositionMappingArray.Length; i++)
+            {
+                charToPositionMappingArray[i] = new List<(int x, int y)>();
+            }
+            for (int x = 0; x < board.Length; x++)
+            {
+                gotThere[x] = new bool[boardWidth];
+                for (int y = 0; y < board[x].Length; y++)
+                {
+                    charToPositionMappingArray[board[x][y] - 65].Add((x, y));
+                }
+            }
+
+            foreach (var word in words)
+            {
+                char[] wordChars = word.ToCharArray();
+                Stack<(int x, int y, char c, int inWordIdx)> reachableCharStack = new Stack<(int x, int y, char c, int inWordIdx)>();
+                Stack<(int x, int y, char c, int inWordIdx)> path = new Stack<(int x, int y, char c, int inWordIdx)>();
+                (int x, int y, char c, int inWordIdx) currentCharInfo, backTrackInfo;
+
+                // dla każdej możliwej ścieżki dla słowa
+                foreach (var (x, y) in charToPositionMappingArray[wordChars[0] - 65])
+                {
+                    reachableCharStack.Push((x, y, wordChars[0], 0));
+                }
+
+                if (reachableCharStack.Count > 0)
+                {
+                    if (word.Length == 1)
+                    {
+                        assemblyableWordList.Add(word);
+                    }
+                    else
+                    {
+                        int nextCharIdx = 1;
+                        char nextChar = wordChars[nextCharIdx];
+                        bool nextCharFound;
+
+                        do
+                        {
+                            currentCharInfo = reachableCharStack.Pop();
+                            
+                            if (path.Count > 0)
+                            {
+                                while (path.Count > 0 && path.Peek().inWordIdx >= currentCharInfo.inWordIdx)
+                                {
+                                    backTrackInfo = path.Pop();
+                                    gotThere[backTrackInfo.x][backTrackInfo.y] = false;
+                                } 
+                            }
+
+                            if (currentCharInfo.inWordIdx == word.Length - 1)
+                            {
+                                assemblyableWordList.Add(word);
+                            }
+                            else
+                            {
+                                path.Push(currentCharInfo);
+                                gotThere[currentCharInfo.x][currentCharInfo.y] = true;
+                                nextCharIdx = currentCharInfo.inWordIdx + 1;
+                                nextChar = wordChars[nextCharIdx];
+
+                                nextCharFound = false;
+                                // Add every possible move to the next letter
+                                for (int x = Math.Max(0, currentCharInfo.x - 1); x < Math.Min(currentCharInfo.x + 1 + 1, boardHeight); x++)
+                                {
+                                    for (int y = Math.Max(0, currentCharInfo.y - 1); y < Math.Min(currentCharInfo.y + 1 + 1, boardWidth); y++)
+                                    {
+                                        if (gotThere[x][y] == false && board[x][y] == nextChar)
+                                        {
+                                            nextCharFound = true;
+                                            reachableCharStack.Push((x, y, nextChar, nextCharIdx));
+                                        }
+                                    }
+                                }
+
+                                if (!nextCharFound)
+                                {
+                                    gotThere[currentCharInfo.x][currentCharInfo.y] = false;
+                                    nextChar = wordChars[nextCharIdx - 1];
+                                    path.Pop();
+                                }
+                            }
+                        } while (reachableCharStack.Count > 0 && currentCharInfo.inWordIdx < word.Length - 1);
+
+                        while (path.Count > 0)
+                        {
+                            currentCharInfo = path.Pop();
+                            gotThere[currentCharInfo.x][currentCharInfo.y] = false;
+                        }
+
+                        while (reachableCharStack.Count > 0)
+                        {
+                            currentCharInfo = reachableCharStack.Pop();
+                            gotThere[currentCharInfo.x][currentCharInfo.y] = false;
+                        }
+                    }
+                }
+            }
+
+            return assemblyableWordList.ToArray();
         }
     }
 }
