da2ae8baa7a247972d9909e25207d71dbd3d4af5
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-29 20:35:31 +0100

    Add - generate from template

---
+++ Add.Tests/SolutionTest.cs
@@ -0,0 +1,22 @@
+using Add.Tests.TestData;
+using Xunit;
+
+namespace Add.Tests
+{
+    public class SolutionTest
+    {
+        [Theory]
+        [ClassData(typeof(ShouldSumTestData))]
+        public void ShouldSum(int param1, int param2, int expectedResult)
+        {
+            // Arrange
+
+            // Act
+            int result = Solution.add(param1, param2);
+
+            // Assert
+            Assert.Equal(expectedResult, result);
+
+        }
+    }
+}
---
+++ Add/Solution.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Add
+{
+    public class Solution
+    {
+        public static int add(int param1, int param2)
+        {
+            return (int)new object();
+        }
+    }
+}
