4e8f697cc24f41ae1f135bd27205bf0c78be7f1c
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-29 20:39:12 +0100

    Add - solution.

--- Add/Solution.cs
+++ Add/Solution.cs
@@ -5,7 +5,7 @@ namespace Add
     {
         public static int add(int param1, int param2)
         {
-            return (int)new object();
+            return param1 + param2;
         }
     }
 }
