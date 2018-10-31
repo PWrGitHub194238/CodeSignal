a0af0d2b083cb03db50523e8827ee215751cbab2
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-10-31 22:52:32 +0100

    MakeArrayConsecutive2 - solution.

--- MakeArrayConsecutive2.Tests/TestData/ShouldGetMinimumNumberOfStatuesTestData.cs
+++ MakeArrayConsecutive2.Tests/TestData/ShouldGetMinimumNumberOfStatuesTestData.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 
 namespace MakeArrayConsecutive2.Tests.TestData
 {
-    class ShouldGetMinimumNumberOfStatuesTestData : IEnumerable<object[]>
+    internal class ShouldGetMinimumNumberOfStatuesTestData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-			// Inputs: int[] statues
-			// Outputs: int
-            yield return new object[] { new int[] { }, 0 };
+            // Inputs: int[] statues
+            // Outputs: int
+            yield return new object[] { new int[] { 6, 2, 3, 8 }, 3 };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
--- MakeArrayConsecutive2/Solution.cs
+++ MakeArrayConsecutive2/Solution.cs
@@ -5,7 +5,41 @@ namespace MakeArrayConsecutive2
     {
         public static int makeArrayConsecutive2(int[] statues)
         {
-            return (int)new object();
+            int smallestStatueSize = 0;
+            int biggestStatusSize = 0;
+            bool[] hasStatueOfSize = new bool[20];
+            int missingStatues = 0;
+
+            for (int idx = 0; idx < statues.Length; idx++)
+            {
+                hasStatueOfSize[statues[idx]] = true;
+            }
+
+            for (int idx = 0; idx < hasStatueOfSize.Length; idx++)
+            {
+                if (hasStatueOfSize[idx] == true)
+                {
+                    smallestStatueSize = idx;
+                    break;
+                }
+            }
+            for (int idx = hasStatueOfSize.Length - 1; idx >= 0; idx -= 1)
+            {
+                if (hasStatueOfSize[idx] == true)
+                {
+                    biggestStatusSize = idx;
+                    break;
+                }
+            }
+
+            for (int idx = smallestStatueSize; idx <= biggestStatusSize; idx++)
+            {
+                if (hasStatueOfSize[idx] == false)
+                {
+                    missingStatues += 1;
+                }
+            }
+            return missingStatues;
         }
     }
 }
