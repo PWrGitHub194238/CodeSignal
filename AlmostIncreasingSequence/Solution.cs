bf9e28c38d61e643cff9ff6d865cd6b5194c9fd6
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-11-01 17:17:33 +0100

    AlmostIncreasingSequence - solution.

--- AlmostIncreasingSequence/Solution.cs
+++ AlmostIncreasingSequence/Solution.cs
@@ -5,7 +5,35 @@ namespace AlmostIncreasingSequence
     {
         public static bool almostIncreasingSequence(int[] sequence)
         {
-            return (bool)new object();
+            int sequenceLength = sequence.Length;
+            int idx = 1;
+
+            while (idx < sequenceLength && sequence[idx - 1] < sequence[idx])
+            {
+                idx += 1;
+            }
+
+            // Entire seqience is strictly increasing or only the last element violates desired property
+            if (idx >= sequenceLength - 1)
+            {
+                return true;
+            }
+            else if (idx > 1)
+            {
+                if (sequence[idx - 2] >= sequence[idx] && sequence[idx - 1] >= sequence[idx + 1])
+                {
+                    return false;
+                }
+            }
+
+            idx += 1;
+
+            while (idx < sequenceLength && sequence[idx - 1] < sequence[idx])
+            {
+                idx += 1;
+            }
+
+            return idx == sequenceLength;
         }
     }
 }
