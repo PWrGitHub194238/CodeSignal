88b9bf3b76c8078426791e392bd27349c5b44290
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-11-03 16:38:53 +0100

    MatrixElementsSum - solution.

--- MatrixElementsSum.Tests/TestData/ShouldReturnSuitableRoomsTotalCostTestData.cs
+++ MatrixElementsSum.Tests/TestData/ShouldReturnSuitableRoomsTotalCostTestData.cs
@@ -3,16 +3,49 @@ using System.Collections.Generic;
 
 namespace MatrixElementsSum.Tests.TestData
 {
-    class ShouldReturnSuitableRoomsTotalCostTestData : IEnumerable<object[]>
+    internal class ShouldReturnSuitableRoomsTotalCostTestData : IEnumerable<object[]>
     {
         public IEnumerator<object[]> GetEnumerator()
         {
-			// Inputs: int[][] matrix
-			// Outputs: int
+            // Inputs: int[][] matrix
+            // Outputs: int
             yield return new object[] { new int[][] { }, 0 };
+            yield return new object[] { new int[][] {
+                new int[] { 0 } }, 0 };
+            yield return new object[] { new int[][] {
+                new int[] { 0, 0, 0 } }, 0 };
+            yield return new object[] { new int[][] {
+                new int[] { 0 },
+                new int[] { 1 } }, 0 };
+            yield return new object[] { new int[][] {
+                new int[] { 1 },
+                new int[] { 0 } }, 1 };
+            yield return new object[] { new int[][] {
+                new int[] { 0, 1, 0 } }, 1 };
+            yield return new object[] { new int[][] {
+                new int[] { 1, 0, 1 } }, 2 };
+            yield return new object[] { new int[][] {
+                new int[] { 1, 0, 1 },
+                new int[] { 0, 1, 0 },
+                new int[] { 1, 0, 1 }}, 2 };
+            yield return new object[] { new int[][] {
+                new int[] { 1, 1, 1 },
+                new int[] { 0, 1, 1 },
+                new int[] { 1, 0, 1 }}, 6 };
+            yield return new object[] { new int[][] {
+                new int[] { 0, 1, 1, 2 },
+                new int[] { 0, 5, 0, 0 },
+                new int[] { 2, 0, 3, 3 } }, 9 };
+            yield return new object[] { new int[][] {
+                new int[] { 1, 1, 1, 0 },
+                new int[] { 0, 5, 0, 1 },
+                new int[] { 2, 1, 3, 10 } }, 9 };
+            yield return new object[] { new int[][] {
+                new int[] { 1, 1, 1 },
+                new int[] { 2, 2, 2 },
+                new int[] { 3, 3, 3 } }, 18 };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
-
--- MatrixElementsSum/Solution.cs
+++ MatrixElementsSum/Solution.cs
@@ -5,7 +5,30 @@ namespace MatrixElementsSum
     {
         public static int matrixElementsSum(int[][] matrix)
         {
-            return (int)new object();
+            if (matrix.Length == 0) return 0;
+
+            int matrixWidth = matrix[0].Length;
+            int matrixHeight = matrix.Length;
+
+            bool[] roomsInColumnAreHaunted = new bool[matrixWidth];
+
+            int totalPrice = 0;
+
+            for (int i = 0; i < matrixHeight; i++)
+            {
+                for (int j = 0; j < matrixWidth; j++)
+                {
+                    if (roomsInColumnAreHaunted[j] == false && matrix[i][j] == 0)
+                    {
+                        roomsInColumnAreHaunted[j] = true;
+                    }
+                    else if (roomsInColumnAreHaunted[j] == false)
+                    {
+                        totalPrice += matrix[i][j];
+                    }
+                }
+            }
+            return totalPrice;
         }
     }
 }
