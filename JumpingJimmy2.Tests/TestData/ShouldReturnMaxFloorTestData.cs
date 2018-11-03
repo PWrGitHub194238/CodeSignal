f9fd5417d75a63bf2c351858fdb1cd1db7140d04
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-11-03 18:51:41 +0100

    JumpingJimmy2 - solution.

--- JumpingJimmy2.Tests/TestData/ShouldReturnMaxFloorTestData.cs
+++ JumpingJimmy2.Tests/TestData/ShouldReturnMaxFloorTestData.cs
@@ -9,7 +9,15 @@ namespace JumpingJimmy2.Tests.TestData
         {
 			// Inputs: int[] tower, int[] power, int[] poison, int jumpHeight
 			// Outputs: int
-            yield return new object[] { new int[] { }, new int[] { }, new int[] { }, 0, 0 };
+            yield return new object[] { new int[] { 1, 4, 3, 2, 2, 2, 2, 1, 4, 4, 2, 3, 3, 4, 1, 4, 2, 1, 2, 4, 1, 2, 2, 4, 1 },
+                new int[] { 1, 3, 11 }, new int[] { 2, 4, 5, 7, 12, 20, 22 }, 4, 56 };
+            yield return new object[] { new int[] { 1, 9, 15, 21, 36 },
+                new int[] { }, new int[] { 0, 1, 2, 3, 4 }, 9, 1 };
+            yield return new object[] { new int[] { 2, 6, 2, 2, 2, 3, 8, 9, 7, 5, 2, 3, 5, 7, 3, 3, 2, 4, 4, 7, 5, 5, 5, 7, 4,
+                5, 10, 2, 10, 10, 3, 5, 2, 9, 2, 6, 7, 8, 2, 8, 8, 3, 6, 8, 9, 3, 2, 5, 6, 6, 4, 2, 1, 4, 1, 8, 9, 2, 4, 9, 9,
+                5, 2, 6, 3, 5, 5, 3, 3, 3, 7, 8, 9, 7, 5, 4, 10, 1, 4, 1, 2, 9, 2, 7, 3, 6, 1, 4, 9, 6, 8, 9, 7, 3, 5, 2, 8 },
+                new int[] { 4, 10, 12, 19, 26, 33, 35, 40, 43, 46, 47, 51, 69, 71, 73, 74, 75, 79, 80, 93 },
+                new int[] { 1, 3, 11, 50, 56, 65, 81, 88, 94 }, 10, 492 };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
--- JumpingJimmy2/Solution.cs
+++ JumpingJimmy2/Solution.cs
@@ -5,7 +5,47 @@ namespace JumpingJimmy2
     {
         public static int jumpingJimmy2(int[] tower, int[] power, int[] poison, int jumpHeight)
         {
-            return (int)new object();
+            int maxHeight = 0;
+            int currentFloorIdx = 0;
+            int nearestPowerFloorIdx = 0;
+            int nearestPoisonFloorIdx = 0;
+
+            int towerMaxFloor = tower.Length;
+            int powerFloorConut = power.Length;
+            int poisonFloorCount = poison.Length;
+
+            int jumpStrength;
+
+            while (currentFloorIdx < towerMaxFloor && jumpHeight >= tower[currentFloorIdx])
+            {
+                jumpStrength = jumpHeight;
+                while (currentFloorIdx < towerMaxFloor && jumpStrength >= tower[currentFloorIdx])
+                {
+                    jumpStrength -= tower[currentFloorIdx];
+                    maxHeight += tower[currentFloorIdx];
+                    currentFloorIdx += 1;
+                }
+
+
+                while (nearestPowerFloorIdx < powerFloorConut && currentFloorIdx - 1 > power[nearestPowerFloorIdx])
+                {
+                    nearestPowerFloorIdx += 1;
+                }
+                while (nearestPoisonFloorIdx < poisonFloorCount && currentFloorIdx - 1 > poison[nearestPoisonFloorIdx])
+                {
+                    nearestPoisonFloorIdx += 1;
+                }
+
+                if (nearestPowerFloorIdx < powerFloorConut && currentFloorIdx - 1 == power[nearestPowerFloorIdx])
+                {
+                    jumpHeight += 1;
+                } else if (nearestPoisonFloorIdx < poisonFloorCount && currentFloorIdx - 1 == poison[nearestPoisonFloorIdx])
+                {
+                    jumpHeight -= 1;
+                }
+            }
+
+            return maxHeight;
         }
     }
 }
