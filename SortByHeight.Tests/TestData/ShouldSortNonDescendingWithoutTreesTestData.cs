e0cd01b2957429928b42372c8258b6efe32e5449
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2018-11-09 21:12:07 +0100

    SortByHeight - solution.

--- SortByHeight.Tests/TestData/ShouldSortNonDescendingWithoutTreesTestData.cs
+++ SortByHeight.Tests/TestData/ShouldSortNonDescendingWithoutTreesTestData.cs
@@ -9,7 +9,11 @@ namespace SortByHeight.Tests.TestData
         {
 			// Inputs: int[] a
 			// Outputs: int[]
-            yield return new object[] { new int[] { }, new int[] { } };
+            yield return new object[] { new int[] { -1, 150, 190, 170, -1, -1, 160, 180 },
+                new int[] { -1, 150, 160, 170, -1, -1, 180, 190 } };
+
+            yield return new object[] { new int[] { 4, 2, 9, 11, 2, 16 },
+                new int[] { 2, 2, 4, 9, 11, 16 } };
         }
 
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
--- SortByHeight/Solution.cs
+++ SortByHeight/Solution.cs
@@ -1,11 +1,56 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SortByHeight
 {
     public class Solution
     {
         public static int[] sortByHeight(int[] a)
         {
-            return (int[])new object();
+            int arrayLength = a.Length;
+            Queue<int> fixedElements = new Queue<int>();
+            ICollection<int> elementsToSort = new List<int>();
+
+            for (int i = 0; i < arrayLength; i += 1)
+            {
+                if (a[i] == -1)
+                {
+                    fixedElements.Enqueue(i);
+                }
+                else
+                {
+                    elementsToSort.Add(a[i]);
+                }
+            }
+
+            int idx = 0;
+            int jdx = 0;
+            int fixedElement;
+            int[] sortedElementsArray = elementsToSort.ToArray();
+            Array.Sort(sortedElementsArray);
+
+            while (fixedElements.Count > 0)
+            {
+                fixedElement = fixedElements.Dequeue();
+                while (idx < fixedElement)
+                {
+                    a[idx] = sortedElementsArray[jdx];
+                    idx += 1;
+                    jdx += 1;
+                }
+                a[idx] = -1;
+                idx += 1;
+            }
+
+            while (idx < arrayLength)
+            {
+                a[idx] = sortedElementsArray[jdx];
+                idx += 1;
+                jdx += 1;
+            }
+
+            return a;
         }
     }
 }
